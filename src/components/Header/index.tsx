3e81bf8ea4ecb6b3bec886049d914ec65e4c781c
Shawon <chiefmahedi@gmail.com>
2024-01-03 18:34:48 +0600

    Add Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,11 @@ export default function Header()
     <>
     <header className={style['header']}>
         <div className={style['logo-container']}>
-            <h1>SortVizPlayground</h1>
+            <h1 className={style['logo']}><span className={style['logo-sort']}>Sort</span>VizPlayground</h1>
+        </div>
+        <div className={style['avatar-container']}>
+            <h2 className={style['avatar-title']}>Developed by, Mahedi Hassan Shawon</h2>
+            <img src='https://res.cloudinary.com/dncupqjwo/image/upload/v1703416683/avatar_fztd3e.png' className={style['avatar']} width="32px" height="32px"/>
         </div>
     </header>
     </>
