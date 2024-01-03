f9a09b7391cf8552b4842dd833a34cb819abc1e9
Shawon <chiefmahedi@gmail.com>
2024-01-04 00:57:51 +0600

    Add Main section component

---
+++ src/components/Body/Main/index.tsx
@@ -0,0 +1,11 @@
+import style from './index.module.css';
+export default function Main()
+{
+    return(
+        <>
+        <main className={style["main-content"]}>
+            <p>Hello</p>
+        </main>
+        </>
+    )
+}
