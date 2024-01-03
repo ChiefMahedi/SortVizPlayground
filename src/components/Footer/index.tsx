5a4f04e0210e2483879524415bbfbad1d509da9d
Shawon <chiefmahedi@gmail.com>
2024-01-04 01:00:24 +0600

    Add Footer section component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import style from './index.module.css';
+export default function Footer()
+{
+    return(
+        <>
+        <footer className={style["footer"]}>
+            <p>Hello</p>
+        </footer>
+        </>
+    )
+}
