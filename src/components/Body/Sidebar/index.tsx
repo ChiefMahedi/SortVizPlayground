a3b69c82e71b52993ff0a50bb577f4a46c095ffd
Shawon <chiefmahedi@gmail.com>
2024-01-04 00:58:36 +0600

    Add Sidebar section component

---
+++ src/components/Body/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+import style from './index.module.css';
+export default function Sidebar()
+{
+    return(
+        <>
+        <aside className={style["sidebar-content"]}>
+            <p>Hello</p>
+        </aside>
+        </>
+    )
+}
