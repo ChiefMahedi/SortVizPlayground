b2ce979560f7ea4ad0743d1123f18eb7a42cb3c9
Shawon <chiefmahedi@gmail.com>
2024-01-05 21:09:40 +0600

    Update Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,15 @@
 import style from './index.module.css';
-export default function Footer()
-{
-    return(
+export default function Footer() {
+    return (
         <>
-        <footer className={style["footer"]}>
-            <p>Hello</p>
-        </footer>
+            <footer className={style["footer"]}>
+                <p>
+                    <a href="https://github.com/ChiefMahedi/SortVizPlayground" style={{textDecoration:"none", color:"gray"}} target="_blank" rel="noopener noreferrer">
+                        GitHub
+                    </a>{" "}
+                    | © {new Date().getFullYear()} Mahedi Hassan Shawon
+                </p>
+            </footer>
         </>
     )
 }
