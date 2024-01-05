ae69ffc4a1380f459b6a9cbd36b4e60ea7d46145
Shawon <chiefmahedi@gmail.com>
2024-01-05 15:25:34 +0600

    Update Body component

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,13 +1,22 @@
 import Main from "./Main";
 import Sidebar from "./Sidebar";
-import style from "./index.module.css"
+import style from "./index.module.css";
+import { useState } from "react";
+
 export default function Body() {
+    const [sequence, setSequence] = useState<number[]>([]); 
+    const [sortAlgorithm, setSortAlgorithm] = useState<string>('bubblesort');
     return (
         <>
             <div className={style['main-body']}>
-                <Main />
+                <Main
+                    sequence={sequence}
+                    alterSequence={setSequence}
+                    algorithm={sortAlgorithm}
+                    changeAlgorithm={setSortAlgorithm}
+                />
                 <Sidebar />
             </div>
         </>
-    )
+    );
 }
